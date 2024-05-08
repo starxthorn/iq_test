c6d8c6139ee0565726e0e4ad65f8d94053725bf9
taimoor safdar <starxthorn@gmail.com>
2024-05-08 07:13:26 -0700

    some changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/models/User";
-import { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "nextAuth",
