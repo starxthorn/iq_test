5ad7194adf594ec1790499d26a9a826100414b93
taimoor safdar <starxthorn@gmail.com>
2024-05-08 07:38:23 -0700

    some chnages

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -23,6 +23,4 @@ export const authOptions: NextAuthOptions = {
   ],
 };
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
